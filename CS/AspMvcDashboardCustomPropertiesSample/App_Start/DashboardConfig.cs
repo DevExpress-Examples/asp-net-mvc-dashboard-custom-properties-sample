50ed3e43c983800ae3f6ed8a997e3c03e5c49df1
AlexanderKovalenko <alexander.kovalenko@devexpress.com>
2021-12-27 17:19:43 +0300

    Remove unused datasources

--- CS/AspMvcDashboardCustomPropertiesSample/App_Start/DashboardConfig.cs
+++ CS/AspMvcDashboardCustomPropertiesSample/App_Start/DashboardConfig.cs
@@ -1,13 +1,9 @@
-using System.Web.Routing;
+using DevExpress.DashboardCommon;
 using DevExpress.DashboardWeb;
 using DevExpress.DashboardWeb.Mvc;
 using DevExpress.DataAccess.Sql;
 using System.Web.Hosting;
-using DevExpress.DataAccess.Excel;
-using DevExpress.DashboardCommon;
-using System.Web;
-using System.IO;
-using System.Xml.Linq;
+using System.Web.Routing;
 
 namespace AspMvcDashboardCustomPropertiesSample {
     public static class DashboardConfig {
@@ -40,25 +36,8 @@ namespace AspMvcDashboardCustomPropertiesSample {
                 .Build("Sales Person");
             sqlDataSource.Queries.Add(query);
             dataSourceStorage.RegisterDataSource("sqlDataSource", sqlDataSource.SaveToXml());
-            
-            // Registers an Object data source.
-            DashboardObjectDataSource objDataSource = new DashboardObjectDataSource("Object Data Source");
-            dataSourceStorage.RegisterDataSource("objDataSource", objDataSource.SaveToXml());
-            
-            // Registers an Excel data source.
-            DashboardExcelDataSource excelDataSource = new DashboardExcelDataSource("Excel Data Source");
-            excelDataSource.FileName = HostingEnvironment.MapPath(@"~/App_Data/Sales.xlsx");
-            excelDataSource.SourceOptions = new ExcelSourceOptions(new ExcelWorksheetSettings("Sheet1"));
-            dataSourceStorage.RegisterDataSource("excelDataSource", excelDataSource.SaveToXml());
-            
-            DashboardConfigurator.Default.SetDataSourceStorage(dataSourceStorage);
-            DashboardConfigurator.Default.DataLoading += DataLoading;
-        }
 
-        private static void DataLoading(object sender, DataLoadingWebEventArgs e) {
-            if(e.DataSourceName == "Object Data Source") {
-                e.Data = Invoices.CreateData();
-            }
+            DashboardConfigurator.Default.SetDataSourceStorage(dataSourceStorage);
         }
     }
 }
